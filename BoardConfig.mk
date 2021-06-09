ab60006e1b8b323929761e225af5cbd1be7a249a
YumeMichi <do4suki@gmail.com>
2021-06-09 11:40:22 +0800

    op9r: OnePlus8T -> OnePlus9R.
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I822254fb4b2df7fb0567fad36f3361ca5cbd7f01

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 include device/oneplus/oneplus8/BoardConfig.mk
 
 # OTA
-TARGET_OTA_ASSERT_DEVICE := OnePlus8T,oneplus8t
+TARGET_OTA_ASSERT_DEVICE := OnePlus9R,oneplus9r
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 7516192768
